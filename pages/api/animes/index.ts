8feb766b6d9d96eb5ef4caa593d879c63f1dfca9
BeneX Drake <benex@hotmail.de>
2024-09-23 12:13:15 +0200

    change filter methode for animes

--- pages/api/animes/index.ts
+++ pages/api/animes/index.ts
@@ -7,5 +7,28 @@ export default function handler(
   req: NextApiRequest,
   res: NextApiResponse,
 ) {
+  console.log(req.query)
+
+
+  if(req.query.id != null)
+  {
+    for(let anime of json)
+    {
+      if(anime._id === req.query.id)
+      {
+        res.status(200).json(anime);
+        break
+      }
+    }
+  }
+
+  if(req.query.title != null)
+  {
+    let title = `${req.query.title}`.toLowerCase()
+    let animes = json.filter(x => x.title.toLowerCase().includes(title))
+    res.status(200).json(animes);
+    return
+  }
+
   res.status(200).json(json);
 }
