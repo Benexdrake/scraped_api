857e0ef7a59191bad238ea4a79d3cac875bb7b13
BeneX Drake <benex@hotmail.de>
2024-10-19 20:44:43 +0200

    adding url and change to function

--- lib/exams.ts
+++ lib/exams.ts
@@ -9,9 +9,13 @@ export const exams = [
     {'id': 'az-900', 'exam':az_900}
 ]
 
-export const exam_infos = [
-    {'id': 'cfs-c02', 'name':cfs_c02.name, 'description':cfs_c02.description, 'level': cfs_c02.level, 'type':cfs_c02.type},
-    {'id': 'saa-c03', 'name':saa_c03.name, 'description':saa_c03.description, 'level': saa_c03.level, 'type':saa_c03.type},
-    {'id': 'az-900', 'name':az_900.name, 'description':az_900.description, 'level': az_900.level, 'type':az_900.type}
-]
+
+export function exam_infos(req:any)
+{   
+    return [
+        {'id': 'cfs-c02', 'name':cfs_c02.name, 'description':cfs_c02.description, 'level': cfs_c02.level, 'type':cfs_c02.type, 'url':`${req.headers.host}/api/exams?id=${cfs_c02.id}`},
+        {'id': 'saa-c03', 'name':saa_c03.name, 'description':saa_c03.description, 'level': saa_c03.level, 'type':saa_c03.type, 'url':`${req.headers.host}/api/exams?id=${cfs_c02.id}`},
+        {'id': 'az-900', 'name':az_900.name, 'description':az_900.description, 'level': az_900.level, 'type':az_900.type, 'url':`${req.headers.host}/api/exams?id=${cfs_c02.id}`}
+    ]
+}
 
--- pages/api/exams/index.ts
+++ pages/api/exams/index.ts
@@ -8,7 +8,7 @@ export default function handler(req: NextApiRequest, res: NextApiResponse)
   let id = req.query.id
 
   if(!id)
-    res.status(200).json(exam_infos)
+    res.status(200).json(exam_infos(req))
 
   for(let exam of exams)
   {
