1d6234e21f73eff7b389379cc31a82d5f81fdeea
BeneX Drake <benex@hotmail.de>
2024-10-19 20:29:24 +0200

    adding info and change code

--- lib/exams.ts
+++ lib/exams.ts
@@ -3,10 +3,15 @@ import saa_c03 from '@/public/exam/aws-saa-c03.json'
 import az_900 from '@/public/exam/az-900.json'
 
 
-let exams = [
+export const exams = [
     {'id': 'cfs-c02', 'exam':cfs_c02},
     {'id': 'saa-c03', 'exam':saa_c03},
     {'id': 'az-900', 'exam':az_900}
 ]
 
-export default exams
+export const exam_infos = [
+    {'id': 'cfs-c02', 'name':cfs_c02.name, 'description':cfs_c02.description, 'level': cfs_c02.level, 'type':cfs_c02.type},
+    {'id': 'saa-c03', 'name':saa_c03.name, 'description':saa_c03.description, 'level': saa_c03.level, 'type':saa_c03.type},
+    {'id': 'az-900', 'name':az_900.name, 'description':az_900.description, 'level': az_900.level, 'type':az_900.type}
+]
+
--- pages/api/exams/info.ts
+++ pages/api/exams/info.ts
@@ -1,18 +1,9 @@
 // Next.js API route support: https://nextjs.org/docs/api-routes/introduction
 import type { NextApiRequest, NextApiResponse } from "next";
-import {exams} from "@/lib/exams";
+import {exam_infos} from "@/lib/exams";
 
 
 export default function handler(req: NextApiRequest, res: NextApiResponse)
 {
-  let id = req.query.id
-
-  if(!id)
-    res.status(200).json('Where is the ID?')
-
-  for(let exam of exams)
-  {
-    if(exam.id === id)
-      res.status(200).json(exam)
-  }
+  res.status(200).json(exam_infos)
 }
