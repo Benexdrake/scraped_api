438fa1243355d80cbf58e3d977b4ca35e0b33847
BeneX Drake <benex@hotmail.de>
2024-12-20 10:09:06 +0100

    cleaning

--- pages/api/exams/index.ts
+++ pages/api/exams/index.ts
@@ -1,4 +1,3 @@
-// Next.js API route support: https://nextjs.org/docs/api-routes/introduction
 import type { NextApiRequest, NextApiResponse } from "next";
 import {exams, exam_infos  } from "@/lib/exams";
 
@@ -8,11 +7,11 @@ export default function handler(req: NextApiRequest, res: NextApiResponse)
   let id = req.query.id
 
   if(!id)
-    res.status(200).json(exam_infos(req))
-
-  for(let exam of exams)
   {
-    if(exam.id === id)
-      res.status(200).json(exam)
+    res.status(200).json(exam_infos(req))
+    return;
   }
+
+  let exam = exams.find(x => x.id === id)
+  res.status(200).json(exam)
 }
