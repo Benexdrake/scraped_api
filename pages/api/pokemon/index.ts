aca19ad26fb556a80dae73440d56bb0f9826653b
BeneX Drake <benex@hotmail.de>
2024-12-20 09:41:46 +0100

    adding nr, all, slice and random

--- pages/api/pokemon/index.ts
+++ pages/api/pokemon/index.ts
@@ -3,9 +3,23 @@ import type { NextApiRequest, NextApiResponse } from "next";
 import json from '@/public/scraped/Pokemon.json'
 
 
-export default function handler(
-  req: NextApiRequest,
-  res: NextApiResponse,
-) {
-  res.status(200).json(json);
+export default function handler(req: NextApiRequest, res: NextApiResponse)
+{
+  let nr = req.query.nr as unknown as number;
+  let all = req.query.all as unknown as boolean;
+
+  let pokes = json.filter(x => x.nr == nr)
+  if(pokes.length > 0)
+  {
+    res.status(200).json(pokes);
+    return;
+  }
+  
+  if(all)
+  {
+    res.status(200).json(json.sort(() => .5 - Math.random()));
+    return
+  }
+  
+    res.status(200).json(json.sort(() => .5 - Math.random()).slice(0,50));
 }
